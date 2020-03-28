599de5ad009b82c94b11f3877c0c9cea834872e6
sabiha22 <54326183+sabiha22@users.noreply.github.com>
2020-03-28 11:48:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function change(elem, color, val){
+    //elem.style.backgroundColor = color;
+    if(val==='c'){
+        alert("Thank You!");
+        //document.getElementById("p1").innerHTML = 'Thank You';
+    }
+    else{
+        alert("We will try to improve!");
+        //document.getElementById("p1").innerHTML = 'We will try to imrove!';
+    }
+    
+}
